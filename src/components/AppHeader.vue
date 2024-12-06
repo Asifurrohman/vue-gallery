843cdb4bddd33d451c091668f481036fe84b34f6
Muhammad <akun.asifurrohman@gmail.com>
2024-12-06 19:47:44 +0700

    modify: second commit

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1 class="text-3xl font-bold underline">
+            Image Storage
+        </h1>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/Homepage.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <AppHeader></AppHeader>
+    </div>
+</template>
+
+<script setup>
+import AppHeader from '@/components/AppHeader.vue'
+</script>
+
+<style scoped>
+
+</style>
